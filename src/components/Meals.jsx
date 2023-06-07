3df41d3f6f1aa1ab410adebdbeca3718838f8e68
Clinton Brito <clintoncsb@gmail.com>
2023-06-07 18:47:50 -0300

    feat: filters toggle

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom/';
+import { Redirect } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
 
 export default function Meals({ data }) {

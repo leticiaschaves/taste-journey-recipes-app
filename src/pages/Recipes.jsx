2a148441c4f56a8816779763a54c7b31f8a4f560
Clinton Brito <clintoncsb@gmail.com>
2023-06-07 16:48:24 -0300

    feat: renders 12 recipes per page

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom/';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import Meals from '../components/Meals';
 import Drinks from '../components/Drinks';
 import Footer from '../components/Footer';
 import '../components/footer.css';
+import { fetchRecipes } from '../redux/actions';
 
 export default function Recipes() {
+  const dispatch = useDispatch();
   const { pathname } = useLocation();
+
+  useEffect(() => {
+    const urlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    const urlMeals = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    dispatch(fetchRecipes(pathname === '/drinks' ? urlDrinks : urlMeals));
+  }, []);
 
   const { recipes, searched } = useSelector((state) => state.recipes);
 

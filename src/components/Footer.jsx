f7d41f33027f92a2b67323c3eb0d611a81f157d1
paulowos <paulowos@gmail.com>
2023-06-09 19:21:15 -0300

    fix: global alert bug when clicking the footer buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import meal from '../images/mealIcon.svg';
 import drink from '../images/drinkIcon.svg';
+import { fetchCategories, fetchRecipesByCategory } from '../redux/actions';
 
 function Footer() {
   const history = useHistory();
+  const dispatch = useDispatch();
 
   return (
-    <footer
-      data-testid="footer"
-    >
-      <button type="button" onClick={ () => history.push('/meals') }>
-        <img
-          data-testid="meals-bottom-btn"
-          src={ meal }
-          alt="food"
-        />
+    <footer data-testid="footer">
+      <button
+        type="button"
+        onClick={ () => {
+          const urlMealsCategories = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+          const urlMeals = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+
+          dispatch(fetchCategories(urlMealsCategories));
+          dispatch(fetchRecipesByCategory(urlMeals, true));
+          history.push('/meals');
+        } }
+      >
+        <img data-testid="meals-bottom-btn" src={ meal } alt="food" />
       </button>
-      <button type="button" onClick={ () => history.push('/drinks') }>
-        <img
-          data-testid="drinks-bottom-btn"
-          src={ drink }
-          alt="drink"
-        />
+      <button
+        type="button"
+        onClick={ () => {
+          const urlDrinksCategories = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+          const urlDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+          dispatch(fetchCategories(urlDrinksCategories));
+          dispatch(fetchRecipesByCategory(urlDrinks, true));
+          history.push('/drinks');
+        } }
+      >
+        <img data-testid="drinks-bottom-btn" src={ drink } alt="drink" />
       </button>
     </footer>
   );
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,16 +1,30 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
+import { fetchRecipes } from '../redux/actions';
 
 export default function Meals({ data }) {
-  const { filteredByCategory } = useSelector((state) => state.recipes);
-  const { meals } = data;
+  const dispatch = useDispatch();
+  const { filteredByCategory, searched } = useSelector(
+    (state) => state.recipes,
+  );
+  const values = Object.values(data);
+  const verification = values.every((value) => value !== null);
+  let meals = [];
+  console.log(data);
+  console.log(verification);
 
-  if (!meals) {
+  if (searched && !verification) {
     global.alert('Sorry, we haven\'t found any recipes for these filters.');
+    const mealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    dispatch(fetchRecipes(mealsUrl));
     return null;
+  }
+
+  if (data.meals) {
+    meals = data.meals;
   }
 
   if (meals.length === 1 && filteredByCategory) {

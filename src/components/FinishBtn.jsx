42ac6b89b30c1ebf043d541be3cd9098f4db9b5c
Clinton Brito <clintoncsb@gmail.com>
2023-06-13 17:14:08 -0300

    feat: page RecipeInProgress created v2

--- src/components/FinishBtn.jsx
+++ src/components/FinishBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FinishBtn() {
   return (
-    <button data-testid="finish-recipe-btn">Finish Recipe</button>
+    <button data-testid="finish-recipe-btn">Finish Recipeee</button>
   );
 }
 

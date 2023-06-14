381a9f9654243c1e9d0f113c724c99380ac272ee
Clinton Brito <clintoncsb@gmail.com>
2023-06-14 18:14:42 -0300

    test: categories navegation between drinks and meals

--- src/tests/04.Recipes.test.js
+++ src/tests/04.Recipes.test.js
@@ -1,67 +1,58 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import App from '../App';
 import fetch from '../../cypress/mocks/fetch';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
-import mealCategories from '../../cypress/mocks/mealCategories';
+// import mealCategories from '../../cypress/mocks/mealCategories';
+// import user from '../redux/reducers/userReducer';
 
 describe('Testa o componente Recipes da aplicação', () => {
   beforeEach(async () => {
     jest.spyOn(global, 'fetch');
     global.fetch.mockImplementation(fetch);
-
-    jest.spyOn(global, 'alert');
-    global.alert.mockImplementation(() => { });
-    const { history } = renderWithRouterAndRedux(<App />);
-    const btn = screen.getByTestId('login-submit-btn');
-    const email = screen.getByTestId('email-input');
-    const password = screen.getByTestId('password-input');
-
-    userEvent.type(email, 'user@user.com');
-    userEvent.type(password, 'strongPassword.com');
-
-    await waitFor(() => {
-      userEvent.click(btn);
-      const { pathname } = history.location;
-      expect(pathname).toBe('/meals');
-    });
-    const search = screen.getByTestId('search-top-btn');
-    const profile = screen.getByAltText(/profile-icon/i);
-
-    expect(search).toBeVisible();
-    expect(profile).toBeInTheDocument();
-
-    userEvent.click(search);
   });
 
-  // beforeEach(() => {
-  //   jest.spyOn(global, 'fetch');
-  //   global.fetch = jest.fn().mockResolvedValue({
-  //     json: jest.fn().mockResolvedValue(mealCategories),
-  //   });
-  //   render(<App />);
-  // });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-  // it('Testa se a função handleClear é executada corretamente quando clica no botão "all", mockando os resultados da API', () => {
-  //   renderWithRouterAndRedux(<App />);
-  //   const allButton = screen.getByTestId('All-category-filter');
-  //   const beefButton = screen.getByTestId('Beef-category-filter');
-
-  //   userEvent.click(beefButton);
-  //   userEvent.click(allButton);
-
-  //   waitFor(() => {
-  //     expect(screen.getByText(/kumpir/i)).toBeInTheDocument();
-  //   });
-  // });
-
-  it('testa se o botão Start Recipe está funcionando corretamente', () => {
+  it('Testa navegação dos menus de categorias', async () => {
     renderWithRouterAndRedux(<App />);
-    const recipeCar = screen.getByTestId('3-recipe-card');
-    userEvent.click(recipeCar);
-    waitFor(() => {
-      expect(screen.getByText(/barabam/i)).toBeInTheDocument();
+    const { history } = renderWithRouterAndRedux(<App />);
+    act(() => {
+      history.push('/meals');
     });
+    await screen.findByText('Timbits');
+    const beefCategory = screen.getByTestId('Beef-category-filter');
+    userEvent.click(beefCategory);
+    await screen.findByText('Beef and Mustard Pie');
+    userEvent.click(beefCategory);
+    await screen.findByText('Timbits');
+
+    const drinks = screen.getByTestId('drinks-bottom-btn');
+    userEvent.click(drinks);
+    await screen.findByText('GG');
+
+    const shakeCategory = screen.getByRole('button', { name: /shake/i });
+    userEvent.click(shakeCategory);
+    await screen.findByText('Avalanche');
+    userEvent.click(shakeCategory);
+    await screen.findByText('GG');
+
+    const allCategory = screen.getByRole('button', { name: /all/i });
+    userEvent.click(allCategory);
+    await screen.findByText('GG');
+  });
+
+  it('testa se o botão Start Recipe está funcionando corretamente', async () => {
+    renderWithRouterAndRedux(<App />);
+    const { history, debug } = renderWithRouterAndRedux(<App />);
+    act(() => {
+      history.push('/drinks');
+    });
+    await screen.findByText('GG');
+    debug();
   });
 });

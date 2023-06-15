dadf3ff45abd476226a397b7155bc72236ca10b0
paulowos <paulowos@gmail.com>
2023-06-15 14:57:03 -0300

    feat: create 3 buttons

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,9 +3,19 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
+
+  const {email} = JSON.parse( localStorage.getItem('user'));
+
   return (
     <div>
       <Header title="Profile" />
+
+      <h1 data-testid="profile-email">{email}</h1>
+
+      <button data-testid="profile-done-btn" type="button"> Done Recipes </button>
+      <button data-testid="profile-favorite-btn" type="button"> Favorite Recipes </button>
+      <button data-testid="profile-logout-btn" type="button"> Logout </button>
+
       <Footer />
     </div>
   );
